bff30d7d5114a99262223b1f5aa879ab2e930bc2
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-03-12 17:22:57 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat() {
+    cats.push('Ralph')
+}
+function destructivelyPrependCat() {
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat() {
+    cats.pop([2])
+}
+function destructivelyRemoveFirstCat () {
+    cats.shift([0])
+}
+function appendCat(Broom) {
+   return [...cats, 'Broom']
+}
+function prependCat(Arnold) {
+    return ['Arnold', ...cats]
+}
+function removeLastCat(){
+   return  cats.slice(0, cats.length- 1)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
